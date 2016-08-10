7c50802b4d8af8962294e978aef19797bbfe8c4f
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2016-08-10 14:47:44 +0000

    Update unicorn setting.

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,55 +31,55 @@ set :log_level, :debug
 # cap deployで実行される一連の処理
 namespace :deploy do
   
-  # Unicorn再起動を実行するタスク
-  desc 'Restart application'
-  task :restart do
-    invoke 'unicorn:restart'
-  end
+# Unicorn再起動を実行するタスク
+desc 'Restart application'
+task :restart do
+  invoke 'unicorn:restart'
+end
   
-  # rake db:createを実行するタスク
-  desc 'Create database'
-  task :db_create do
-    on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:create'
-        end
+# rake db:createを実行するタスク
+desc 'Create database'
+task :db_create do
+  on roles(:db) do |host|
+    with rails_env: fetch(:rails_env) do
+      within current_path do
+        execute :bundle, :exec, :rake, 'db:create'
       end
     end
   end
+end
   
-  # rake db:seedを実行するタスク
-  desc 'Run seed'
-  task :seed do
-    on roles(:app) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
-        end
+# rake db:seedを実行するタスク
+desc 'Run seed'
+task :seed do
+  on roles(:app) do
+    with rails_env: fetch(:rails_env) do
+      within current_path do
+        execute :bundle, :exec, :rake, 'db:seed'
       end
     end
   end
+end
   
-  after :publishing, :restart
+after :publishing, :restart
   
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-    end
+after :restart, :clear_cache do
+  on roles(:web), in: :groups, limit: 3, wait: 10 do
   end
+end
 
-  # .env以外のgitignoreファイルをアップロードするタスク
-  desc 'Upload config files'
-    task :config_upload do
-      on roles(:app) do |host|
-        if test "[ ! -d #{shared_path}/config ]"
-          execute "mkdir -p #{shared_path}/config"
-        end
-        upload!('config/database.yml', "#{shared_path}/config/database.yml")
-        upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")      
-    end
+# .env以外のgitignoreファイルをアップロードするタスク
+desc 'Upload config files'
+  task :config_upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/database.yml', "#{shared_path}/config/database.yml")
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")      
   end
-  before :started, 'deploy:config_upload'
+end
+before :started, 'deploy:config_upload'
 end
 
 # .envファイルのみをアップロードするタスク、Deploy前に一度実行する
